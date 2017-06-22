ec4c3e2b5ce986eb9fcbf8b8645c7c3863a7382c
rjmintn <rjmorawski@gmail.com>
2017-06-22 12:41:05 -0400

    assign 16 rails seeding data complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,20 +6,16 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'random_data'
-50.times do
-  Post.create!(
-    title: RandomData.random_sentence,
-    body: RandomData.random_paragraph
-  )
-end
-posts = Post.all
+#50.times do
+  Post.find_or_create_by!(title: "Examples") do |post|
+    post.body = "This file should contain all the record creation needed to seed the database with its default values. The data can then be loaded with the rake db:seed (or created alongside the db with db:setup)."
+  end
+#end
+posts = Post.find_by_title("Examples")
 
-100.times do
-  Comment.create!(
-  post: posts.sample,
-  body: RandomData.random_paragraph
-  )
-end
+  Comment.find_or_create_by!(post_id: posts.id) do |comment|
+    comment.body = "blah blah blab blah blah dee blah"
+  end
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
