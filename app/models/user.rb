b949409fa1711323ec6d565c17fdd5419dd24a0b
rjmintn <rjmorawski@gmail.com>
2017-07-03 12:05:48 -0400

    assign 23 rails auth complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ActiveRecord::Base
+
   before_save { self.email = email.downcase if email.present? }
+
+  before_save do
+    if name
+      name1 = []
+      name.split(' ').each do |n|
+        name1.push(n.capitalize)
+      end
+      self.name = name1.join(' ')
+    end
+  end
+
+
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -10,4 +23,5 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
 end
