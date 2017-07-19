d33ac485dea18aa3b15732faeb1f6f483c7b9f7b
rjmintn <rjmorawski@gmail.com>
2017-07-19 13:14:48 -0400

    chkpt 27 rails posts and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,15 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'random_data'
 
+5.times do
+  User.create!(
+  name:     RandomData.random_name,
+  email:    RandomData.random_email,
+  password: RandomData.random_sentence
+  )
+end
+users = User.all
+
 15.times do
   Topic.create!(
   name: RandomData.random_sentence,
@@ -17,6 +26,7 @@ topics = Topic.all
 
 50.times do
   Post.create!(
+    user: users.sample,
     topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
@@ -30,7 +40,15 @@ posts = Post.all
   body: RandomData.random_paragraph
   )
 end
+
+user = User.first
+user.update_attributes!(
+  email: 'rjmorawski@gmail.com',
+  password: 'helloworld'
+)
+
 puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
