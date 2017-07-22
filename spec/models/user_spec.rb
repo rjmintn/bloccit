d4e1d33fcf2c030e50b92438a11ccbc239ae24b1
rjmintn <rjmorawski@gmail.com>
2017-07-22 13:16:50 -0400

    chkpt 29 rails comments complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
   it { is_expected.to have_many(:posts)}
+  it {is_expected.to have_many(:comments)}
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
@@ -64,7 +65,7 @@ RSpec.describe User, type: :model do
       end
     end
   end
-  
+
   describe "invalid user" do
   let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
   let(:user_with_invalid_email) { User.new(name: "Bloccit User", email: "") }
