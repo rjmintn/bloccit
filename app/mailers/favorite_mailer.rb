c5143878f76b5709d2062ba903e3b0a0825e096d
rjmintn <rjmorawski@gmail.com>
2017-07-24 16:49:44 -0400

    assign 31 rails favorites completed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -11,4 +11,13 @@ class FavoriteMailer < ApplicationMailer
     @comment = comment
     mail(to: user.email, subject: "New comment on #{post.title}")
   end
+
+  def new_post(post)
+    headers["Message-ID"] = "<posts/#{post.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @post = post
+    mail(to: post.user.email, subject: "You have favorited, #{post.title} !")
+  end
 end
